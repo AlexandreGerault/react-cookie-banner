be758d1152820ab1f10386dafe98e85ee7f919ef
Alexandre Gérault <alexandre@hexium.io>
2021-11-08 01:04:55 +0100

    refactor(cookie): rename accepted to submitted

--- src/domain/CookieBanner.ts
+++ src/domain/CookieBanner.ts
@@ -2,30 +2,34 @@ import { CookieOption } from './CookieOption';
 
 export class CookieBanner {
   private _accepted: boolean = false;
-  private _options: CookieOption[] = [];
+  private _options: Set<CookieOption> = new Set();
 
   accepts() {
     this._accepted = true;
   }
 
-  accepted() {
+  submitted() {
     return this._accepted;
   }
 
   addOption(option: CookieOption) {
-    this._options = [...this._options, option];
+    if (this.options().find((o) => option.name() === o.name())) {
+      return;
+    }
+
+    this._options.add(option);
   }
 
   options(): CookieOption[] {
-    return this._options;
+    return [...this._options.values()];
   }
 
-  validatedOptions(): CookieOption[] {
-    return this._options.filter((option) => option.isValidated());
+  acceptedOptions(): CookieOption[] {
+    return this.options().filter((option) => option.isValidated());
   }
 
-  validateOption(name: string) {
-    this._options
+  acceptOption(name: string) {
+    this.options()
       .filter((option) => option.name() === name)
       .map((option) => {
         option.validate();
--- tests/CookieBanner.spec.ts
+++ tests/CookieBanner.spec.ts
@@ -1,72 +1,81 @@
-import {CookieBanner} from '../src/domain/CookieBanner'
+import { CookieBanner } from '../src/domain/CookieBanner';
 import { CookieOption } from '../src/domain/CookieOption';
 
 describe('CookieBanner', () => {
-  it('should be not accepted by default', () => {
+  it('should be not submitted by default', () => {
     const cookieBanner = new CookieBanner();
 
-    const isAccepted = cookieBanner.accepted()
+    const isSubmitted = cookieBanner.submitted();
 
-    expect(isAccepted).toEqual(false)
-  })
+    expect(isSubmitted).toEqual(false);
+  });
 
   it('should be accepted after validation', () => {
-    const cookieBanner = new CookieBanner()
+    const cookieBanner = new CookieBanner();
 
-    cookieBanner.accepts()
+    cookieBanner.accepts();
 
-    expect(cookieBanner.accepted()).toBe(true);
-  })
+    expect(cookieBanner.submitted()).toBe(true);
+  });
 
   it('should be able to register one option', () => {
-    const cookieBanner = new CookieBanner()
+    const cookieBanner = new CookieBanner();
 
-    cookieBanner.addOption(CookieOption.create({name: 'Google Tag Manager'}));
+    cookieBanner.addOption(CookieOption.create({ name: 'Google Tag Manager' }));
 
-    expect(cookieBanner.options()).toHaveLength(1)
-  })
+    expect(cookieBanner.options()).toHaveLength(1);
+  });
 
   it('should be able to register two options', () => {
-    const cookieBanner = new CookieBanner()
+    const cookieBanner = new CookieBanner();
 
-    cookieBanner.addOption(CookieOption.create({name: 'Google Tag Manager'}));
-    cookieBanner.addOption(CookieOption.create({name: 'Google Analytics'}));
+    cookieBanner.addOption(CookieOption.create({ name: 'Google Tag Manager' }));
+    cookieBanner.addOption(CookieOption.create({ name: 'Google Analytics' }));
 
-    expect(cookieBanner.options()).toHaveLength(2)
-  })
+    expect(cookieBanner.options()).toHaveLength(2);
+  });
 
-  it('should be able to validate an option', () => {
-    const cookieBanner = new CookieBanner()
+  it('should be able to accept an option', () => {
+    const cookieBanner = new CookieBanner();
 
-    cookieBanner.addOption(CookieOption.create({name: 'Google Tag Manager'}));
+    cookieBanner.addOption(CookieOption.create({ name: 'Google Tag Manager' }));
 
-    cookieBanner.validateOption("Google Tag Manager")
+    cookieBanner.acceptOption('Google Tag Manager');
 
-    expect(cookieBanner.validatedOptions()).toHaveLength(1)
-  })
+    expect(cookieBanner.acceptedOptions()).toHaveLength(1);
+  });
 
   it('should be able to register two options and validate them', () => {
-    const cookieBanner = new CookieBanner()
+    const cookieBanner = new CookieBanner();
 
-    cookieBanner.addOption(CookieOption.create({name: 'Google Tag Manager'}));
-    cookieBanner.addOption(CookieOption.create({name: 'Google Analytics'}));
+    cookieBanner.addOption(CookieOption.create({ name: 'Google Tag Manager' }));
+    cookieBanner.addOption(CookieOption.create({ name: 'Google Analytics' }));
 
-    cookieBanner.validateOption("Google Tag Manager")
-    cookieBanner.validateOption("Google Analytics")
+    cookieBanner.acceptOption('Google Tag Manager');
+    cookieBanner.acceptOption('Google Analytics');
 
-    expect(cookieBanner.options()).toHaveLength(2)
-    expect(cookieBanner.validatedOptions()).toHaveLength(2)
-  })
+    expect(cookieBanner.options()).toHaveLength(2);
+    expect(cookieBanner.acceptedOptions()).toHaveLength(2);
+  });
 
   it('should be able to register two options and validate one', () => {
-    const cookieBanner = new CookieBanner()
+    const cookieBanner = new CookieBanner();
 
-    cookieBanner.addOption(CookieOption.create({name: 'Google Tag Manager'}));
-    cookieBanner.addOption(CookieOption.create({name: 'Google Analytics'}));
+    cookieBanner.addOption(CookieOption.create({ name: 'Google Tag Manager' }));
+    cookieBanner.addOption(CookieOption.create({ name: 'Google Analytics' }));
 
-    cookieBanner.validateOption("Google Tag Manager")
+    cookieBanner.acceptOption('Google Tag Manager');
 
-    expect(cookieBanner.options()).toHaveLength(2)
-    expect(cookieBanner.validatedOptions()).toHaveLength(1)
-  })
-})
+    expect(cookieBanner.options()).toHaveLength(2);
+    expect(cookieBanner.acceptedOptions()).toHaveLength(1);
+  });
+
+  it('should not be able to register an option twice', () => {
+    const cookieBanner = new CookieBanner();
+
+    cookieBanner.addOption(CookieOption.create({ name: 'Google Tag Manager' }));
+    cookieBanner.addOption(CookieOption.create({ name: 'Google Tag Manager' }));
+
+    expect(cookieBanner.options()).toHaveLength(1);
+  });
+});
